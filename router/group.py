4133f21c91279140699270c99e94167d8927a414
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-11-24 10:47:40 +0530

    added comments

--- router/group.py
+++ router/group.py
@@ -4,11 +4,14 @@ from settings import settings
 from fastapi.responses import JSONResponse
 
 router = APIRouter(prefix="/group", tags=["Group"])
+group_db_url = settings.db_url + "group/"
 
 @router.post("/get-group-data")
-def get_session_data(group_id: str):
-    r = requests.get(settings.db_url + "group/" + group_id)
-    group_data = r.json()
+def get_group_data(group_id: str):
+    """
+    API to get details about a group
+    """
+    group_data = requests.get(group_db_url + group_id).json()
     if "errors" not in group_data:
             return group_data
     return JSONResponse("Group not found")
