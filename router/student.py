e1cfd453a580edce1c89955fc1aaa5af4f72b9c2
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-11-30 19:53:23 +0530

    added id check

--- router/student.py
+++ router/student.py
@@ -21,8 +21,11 @@ def birthdate_matches(student_id:str, birth_date:str):
     """
     API to check if the given birthdate matches to the student ID
     """
-    query_params = {'student_id':student_id, 'date_of_birth': birth_date}
-    response = requests.get(student_db_url, params=query_params)
-    if response.status_code == 200:
-            return response.json()
-    return HTTPException(status_code=response.status_code, detail=response.errors)
+    student_id_exists = student_id_exists(student_id)
+    if student_id_exists:
+        query_params = {'student_id':student_id, 'date_of_birth': birth_date}
+        response = requests.get(student_db_url, params=query_params)
+        if response.status_code == 200:
+                return response.json()
+        return HTTPException(status_code=response.status_code, detail=response.errors)
+    return HTTPException(status_code=404, detail="Student ID does not exist!")
