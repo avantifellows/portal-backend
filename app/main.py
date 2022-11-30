be1ca0c4dcfbd2eeb314606422165e033a6325be
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-11-30 11:20:43 +0530

    updated main.py

--- app/main.py
+++ app/main.py
@@ -14,4 +14,8 @@ def authjwt_exception_handler(request: Request, exc: AuthJWTException):
 
 app.include_router(auth.router)
 
+@app.get("/")
+def index():
+    return "Welcome to Portal!"
+
 handler = Mangum(app)
