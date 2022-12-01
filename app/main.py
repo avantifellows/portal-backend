e1fb28eed074cd5f64a9233aa63ab00274ae68c5
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-12-01 10:10:55 +0530

    changed prod

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from router import auth
+from app.router import auth
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi_jwt_auth.exceptions import AuthJWTException
-from mangum import Mangum
+#from mangum import Mangum
 
 app = FastAPI()
 
@@ -18,4 +18,4 @@ app.include_router(auth.router)
 def index():
     return "Welcome to Portal!"
 
-handler = Mangum(app)
+#handler = Mangum(app)
