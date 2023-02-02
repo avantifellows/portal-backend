3b05948116ae6958beb0a8b54a90a0a863728785
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2023-02-02 12:50:29 +0530

    made a universal API

--- app/router/student.py
+++ app/router/student.py
@@ -1,33 +1,63 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 import requests
 from settings import settings
 
+
 router = APIRouter(prefix="/student", tags=["Student"])
-student_db_url = settings.db_url + "student/"
+student_db_url = settings.db_url + "/student/"
 
 
-@router.post("/student-id-exists")
-def student_id_exists(student_id: str):
+@router.get("/{student_id}")
+def index(student_id: str, request: Request):
     """
-    API to check if a student ID exists
+    This API checks if the provided student ID exists in the database.
+    Additionally, if any other details need to be matched in the database, these details can be sent as query parameters.
+
+    Parameters:
+    student_id (str): The student ID to be checked.
+
+    Optional parameters:
+    Example: birth_date (str), category (str), stream (str).
+    For extensive list of optional parameters, refer to the DB schema note on Notion.
+
+    Returns:
+    list: Returns student details if the student ID exists in the database (and optional parameters match, if they are provided), 404 otherwise.
+
+    Example:
+    > $BASE_URL/student/1234
+    returns [{student_data}]
+
+    > $BASE_URL/student/{invalid_id}
+    returns {
+        "status_code": 404,
+        "detail": "Student ID does not exist!",
+        "headers": null
+    }
+
+    > $BASE_URL/student/1234/?stream=PCB&catgeory=Gen
+    returns [{student_data}]
+
     """
+    extra_parameters = dict(request.query_params)
     query_params = {"student_id": student_id}
     response = requests.get(student_db_url, params=query_params)
+
     if response.status_code == 200:
+        if len(response.json()) == 0:
+            return HTTPException(status_code=404, detail="Student ID does not exist!")
+
+        elif len(extra_parameters) != 0:
+            query_params.update(extra_parameters)
+            response = requests.get(student_db_url, params=query_params)
+            if response.status_code == 200:
+                if len(response.json()) == 0:
+                    return HTTPException(
+                        status_code=404, detail="Paramters do not match!"
+                    )
+                return response.json()
+            return HTTPException(
+                status_code=response.status_code, detail=response.errors
+            )
+
         return response.json()
     return HTTPException(status_code=response.status_code, detail=response.errors)
-
-
-@router.post("/birthdate-matches")
-def birthdate_matches(student_id: str, birth_date: str):
-    """
-    API to check if the given birthdate matches to the student ID
-    """
-    does_student_id_exist = student_id_exists(student_id)
-    if does_student_id_exist:
-        query_params = {"student_id": student_id, "date_of_birth": birth_date}
-        response = requests.get(student_db_url, params=query_params)
-        if response.status_code == 200:
-            return response.json()
-        return HTTPException(status_code=response.status_code, detail=response.errors)
-    return HTTPException(status_code=404, detail="Student ID does not exist!")
