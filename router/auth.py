719e6329b1094dd17b187901d6892a8b66a9c7bb
Bahugunajii <amanbahuguna009@gmail.com>
2023-02-07 12:45:30 +0530

    added pre-flight request handler

--- router/auth.py
+++ router/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from fastapi_jwt_auth import AuthJWT
+from fastapi.responses import JSONResponse
 from models import User
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
@@ -7,11 +8,22 @@ router = APIRouter(prefix="/auth", tags=["Authentication"])
 @router.get("/")
 def index():
     return "Portal Authentication!"
+
+# Handles pre-flight requests
+@router.options("/create-access-token")
+def handle_options_root():
+    response = JSONResponse(content={"message": "Success"})
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+    return response
     
 # if user is valid, generates both access token and refresh token. Otherwise, only an access token.
 @router.post("/create-access-token")
 def create_access_token(user: User, Authorize: AuthJWT = Depends()):
     data = user.data
+    print(user)
+    print(data)
     if user.data is None:
         data = {}
     if user.is_user_valid:
@@ -47,3 +59,4 @@ def logout(Authorize: AuthJWT = Depends()):
     Authorize.jwt_required()
     Authorize.unset_jwt_cookies()
     return {"message": "Successful logout"}
+    
