37f3c7e6b015bd895ec702e650045c230df9bb9f
Bahugunajii <amanbahuguna009@gmail.com>
2023-02-08 13:36:37 +0530

    added origins

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from router import auth
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi_jwt_auth.exceptions import AuthJWTException
+from fastapi.middleware.cors import CORSMiddleware
 import settings
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.exception_handler(AuthJWTException)
 def authjwt_exception_handler(request: Request, exc: AuthJWTException):
