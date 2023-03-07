bc3ed590b52979eac3bade1821bd74b120f2ed7c
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2023-03-07 16:12:22 +0530

    session occurrence

--- app/router/session_group.py
+++ app/router/session_group.py
@@ -8,6 +8,26 @@ session_group_db_url = settings.db_url + "/group-session/"
 
 @router.get("/{session_id}")
 def get_group_for_session(session_id: str):
+    """
+    This API returns group ID corresponding to the provided session ID, if the ID exists in the database
+
+    Parameters:
+    session_id (str): The ID against which group ID needs to be retrieved
+
+    Returns:
+    list: Returns group ID if the session ID exists in the database. If the ID does not exist, 404 is returned
+
+    Example:
+    > $BASE_URL/session/1234
+    returns [{group_id: 123}]
+
+    > $BASE_URL/session/{invalid_id}
+    returns {
+        "status_code": 404,
+        "detail": "Session ID does not exist!",
+        "headers": null
+    }
+    """
     response = requests.get(session_group_db_url + session_id)
     if response.status_code == 200:
         if len(response.json()) != 0:
--- app/router/user_session.py
+++ app/router/user_session.py
@@ -9,6 +9,8 @@ user_session_db_url = settings.db_url + "/user-session/"
 
 @router.post("/")
 def user_session_data(user_session: UserSession):
+    """
+    This API writes user interaction details corresponding to a session ID.
+    """
     response = requests.post(user_session_db_url, data=user_session)
-    if response.status_code != 200:
-        return HTTPException(status_code=response.status_code, detail=response.errors)
+    return response
