8b24a4bb374bc828f68adaf1febf683685ed0de1
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2023-03-22 11:26:30 +0530

    env var is not being detected by lambda

--- app/router/session_occurrence.py
+++ app/router/session_occurrence.py
@@ -10,6 +10,7 @@ session_occurrence_db_url = settings.db_url + "/session-occurrence/"
 
 print(session_occurrence_db_url)
 
+
 def build_date_and_time(date_time: str):
     """
     Parses string into date and time strings
