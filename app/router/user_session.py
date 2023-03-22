b3eb89f3d6e06e444881224df18bbf137ecb6afe
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2023-03-22 16:23:11 +0530

    creates user session resource

--- app/router/user_session.py
+++ app/router/user_session.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, HTTPException
 import requests
 from settings import settings
 from models import UserSession
+from datetime import datetime
+from fastapi.responses import JSONResponse
 
 router = APIRouter(prefix="/user-session", tags=["User-Session"])
 user_session_db_url = settings.db_url + "/user-session/"
@@ -12,5 +14,16 @@ def user_session_data(user_session: UserSession):
     """
     This API writes user interaction details corresponding to a session ID.
     """
-    response = requests.post(user_session_db_url, data=user_session)
-    return response
+    query_params = {}
+    user = user_session.dict()
+    for key in user:
+        if key == "user_id" or key == "is_user_valid":
+            query_params[key] = str(user[key]).lower()
+        else:
+            query_params[key] = user[key]
+
+    query_params["start_time"] = datetime.now().isoformat()
+    response = requests.post(user_session_db_url, data=query_params)
+    if response.status_code == 201:
+        return JSONResponse(status_code=201, content="Resource created successfully")
+    raise HTTPException(status_code=500, detail="Resource could not be created!")
