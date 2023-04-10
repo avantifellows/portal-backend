9621829a4f85e6a15ce73b85efda357b0a4250c1
Bahugunajii <amanbahuguna009@gmail.com>
2023-04-10 13:23:20 +0530

    suggested changes

--- app/router/group.py
+++ app/router/group.py
@@ -31,7 +31,7 @@ def get_group_data(request: Request):
     """
     query_params = {}
     for key in request.query_params.keys():
-        if key not in ["name", "id", "input_schema", "locale", "locale_data"]:
+        if key not in ["name", "id", "locale"]:
             raise HTTPException(
                 status_code=400, detail="Query Parameter {} is not allowed!".format(key)
             )
