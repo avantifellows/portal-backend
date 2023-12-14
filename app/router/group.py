24a4e4134e7b818d5ecdb966a88fa88d27c7b4a7
Krrupa <25496389+sudheshna-donthineni@users.noreply.github.com>
2023-12-14 18:21:21 +0530

    Update group.py

--- app/router/group.py
+++ app/router/group.py
@@ -35,7 +35,7 @@ def get_group_data(request: Request):
         request.query_params, mapping.GROUP_QUERY_PARAMS
     )
     response = requests.get(
-        routes.group_type_db_url, params=query_params, headers=db_request_token()
+        routes.group_db_url, params=query_params, headers=db_request_token()
     )
     if helpers.is_response_valid(response, "Group API could not fetch the data!"):
         return helpers.is_response_empty(
