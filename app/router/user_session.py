a258ab5c1f7bcff432fd0fef11f5db5ccde00cb9
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2024-05-25 14:53:34 +0530

    user-session API

--- app/router/user_session.py
+++ app/router/user_session.py
@@ -4,7 +4,7 @@ from models import UserSession
 from datetime import datetime
 from routes import user_session_db_url
 from helpers import db_request_token, is_response_valid, is_response_empty
-from router import  student, session, user
+from router import student, session, user
 from request import build_request
 
 router = APIRouter(prefix="/user-session", tags=["User-Session"])
@@ -15,12 +15,16 @@ async def user_session(user_session: UserSession):
     query_params = user_session.dict()
     query_params["timestamp"] = datetime.now().isoformat()
 
-    if query_params['user_type'] == 'student':
-        user_id_response = student.get_students(build_request(query_params={"student_id": query_params["user_id"]}))
+    if query_params["user_type"] == "student":
+        user_id_response = student.get_students(
+            build_request(query_params={"student_id": query_params["user_id"]})
+        )
 
     query_params["user_id"] = user_id_response[0]["user"]["id"]
 
-    session_pk_id_response = await session.get_session(build_request(query_params={"session_id": query_params["session_id"]}))
+    session_pk_id_response = await session.get_session(
+        build_request(query_params={"session_id": query_params["session_id"]})
+    )
     query_params["session_id"] = session_pk_id_response[0]["id"]
 
     response = requests.post(
