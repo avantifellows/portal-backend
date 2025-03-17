75b568b7637784f5e2d06dd1d3b6c0bc70586c23
suryabulusu <teja.surya59@gmail.com>
2025-03-18 00:08:18 +0530

    precommit errors

--- app/router/abtest.py
+++ app/router/abtest.py
@@ -5,17 +5,22 @@ from helpers import validate_and_build_query_params
 
 router = APIRouter(prefix="/abtest", tags=["ABTest"])
 
+
 @router.get("/")
 def get_ab_test_data(request: Request):
     try:
         query_params = validate_and_build_query_params(request.query_params, ["id"])
         file_path = Path(f"public/abtests/{query_params['id']}.json")
         if not file_path.exists():
-            raise HTTPException(status_code=404, detail=f"A/B test with ID {id} not found")
+            raise HTTPException(
+                status_code=404, detail=f"A/B test with ID {id} not found"
+            )
         with open(file_path, "r") as file:
             ab_test_data = json.load(file)
         return ab_test_data
     except json.JSONDecodeError:
         raise HTTPException(status_code=500, detail="Error parsing A/B test data")
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error retrieving A/B test data: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Error retrieving A/B test data: {str(e)}"
+        )
